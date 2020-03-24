bfceca7520a544cae93f8353813df52c9ccddbcf
Daniel <disaavedra5@gmail.com>
2020-03-24 05:27:36 -0300

    Parte 1

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -6,8 +6,18 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Daniel");
+            Persona lanzamiento;
+            lanzamiento = new Persona();
+            Console.WriteLine(lanzamiento.lanzar());
+        }
+    }
+    class Persona
+    {
+        public int lanzar()
+        {
+            int randomInt;
+            randomInt = new Random().Next(0, 3);
+            return randomInt;
         }
     }
 }
